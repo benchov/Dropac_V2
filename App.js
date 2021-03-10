5bed2b3252b07cd918efdf464beb6a26896631ea
benchov <viktor.mihalyi@yahoo.com>
2021-03-10 15:37:31 +0100

    refactor(App.js): Add auto format related changes

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -9,7 +9,6 @@ import {
 } from 'react-native';
 import dgram from 'react-native-udp';
 import _ from 'lodash';
-import stateParser from './util/index';
 import { stateParser, filterAndRound } from './util';
 
 const App = () => {

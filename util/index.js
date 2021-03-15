686c461fed7bc4604170844cb401dab8419b4e1c
benchov <viktor.mihalyi@yahoo.com>
2021-03-15 16:00:29 +0100

    refactor(util/index.js): Prettier related changes

--- util/index.js
+++ util/index.js
@@ -1,20 +1,30 @@
 import _ from 'lodash';
 
 export const stateParser = (msg) => {
-    const buffer = _.map(_.split(msg, ';'), (state) => {
-        if (state !== '') {
-            return _.split(state, ':');
-        }
-    });
-    const buf2 = _.pull(buffer, undefined);
-    return _.fromPairs(buf2, 'agx');
+  const buffer = _.map(_.split(msg, ';'), (state) => {
+    if (state !== '') {
+      return _.split(state, ':');
+    }
+  });
+  const buf2 = _.pull(buffer, undefined);
+  return _.fromPairs(buf2, 'agx');
 };
 
-export const filterAndRound = (number)=> {
-    const buffer = Math.floor(number * 1000) / 100;
-    
-    if ( (buffer > 10 && buffer > 0) || (buffer < -10 && buffer < 0) ) {
-        return Math.round(buffer)
-    }
-    return 0;
-}
+export const filterAndRound = (number, type) => {
+  const buffer = Math.floor(number * 1000) / 100;
+
+  if (type === 'gyro') {
+    // if (buffer > 8 && buffer > 0) {
+    //   return 50;
+    // }
+    // if (buffer < -8 && buffer < 0) {
+    //   return -50;
+    // }
+    return Math.round(buffer) * 5;
+  }
+
+  if ((buffer > 10 && buffer > 0) || (buffer < -10 && buffer < 0)) {
+    return Math.round(buffer);
+  }
+  return 0;
+};

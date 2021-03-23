6513beaaa582520287594e4a95e3971c444e0173
benchov <viktor.mihalyi@yahoo.com>
2021-03-23 11:44:08 +0100

    feat(App.js): Add second argument to handle more cases

--- components/AltButton.js
+++ components/AltButton.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import {StyleSheet, View, Pressable, Text} from 'react-native';
 
+// TODO :: Rename function, the name is too specific
 const AltButton = (props) => {
   return (
     <View>
-      <Pressable onPressIn={() => props.pressed(props.name)} onPressOut={() => props.pressed(false)}>
+      <Pressable onPressIn={() => props.pressed(true, props.name)} onPressOut={() => props.pressed(false)}>
           <View style={{backgroundColor: 'blue', padding: 40}}><Text>{props.name}</Text></View>
       </Pressable>
     </View>
